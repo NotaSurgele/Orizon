cfd9f67f4e7ef0496f0ee2ca01a4b1fab9c978f9
NotaSurgele <alexis.picard@epitech.eu>
2022-10-24 18:56:04 +0400

    can now add actions related to input

--- src/Engine/Input.cpp
+++ src/Engine/Input.cpp
@@ -2,14 +2,14 @@
 #include <algorithm>
 #include <SFML/System.hpp>
 
-bool Input::isKeyPressed(Key key)
+bool Input::isKeyPressed(std::string key)
 {
     return std::find(___keyArray.begin(), ___keyArray.end(), key) != ___keyArray.end();
 }
 
 void Input::___push_key(sf::Keyboard::Key key)
 {
-    Key key_pressed = _key_map[key];
+    std::string key_pressed = _sfml_to_key[key];
 
     std::cout << "Push input nb : " << key_pressed << std::endl;
     Input::___keyArray.push_back(key_pressed);
@@ -17,11 +17,23 @@ void Input::___push_key(sf::Keyboard::Key key)
 
 void Input::___remove_key(sf::Keyboard::Key key)
 {
-    Key to_remove = _key_map[key];
+    std::string to_remove = _sfml_to_key[key];
+    std::vector<std::string>::iterator it = std::find(___keyArray.begin(), ___keyArray.end(), to_remove);
 
-    std::vector<Key>::iterator it = std::find(___keyArray.begin(), ___keyArray.end(), to_remove);
     if (it != ___keyArray.end()) {
         std::cout << "find input n: " << ___keyArray.at(std::distance(___keyArray.begin(), it)) << std::endl;
         ___keyArray.erase(it);
     }
+}
+
+bool Input::isActionPressed(std::string action)
+{
+    std::string key_related = _action_map[action];
+
+    return Input::isKeyPressed(key_related);
+}
+
+void Input::__add_action(std::string action, std::string name)
+{
+    _action_map.insert(std::pair<std::string, std::string>(action, name));
 }
