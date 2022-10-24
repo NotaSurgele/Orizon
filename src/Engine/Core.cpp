3cc5de90d500f4c267350a15be9e1d8cf3b8184f
NotASurgele <alexis.picard@epitech.eu>
2022-10-24 22:24:40 +0400

    impleted a App class for easier Application creation

--- src/Engine/Core.cpp
+++ src/Engine/Core.cpp
@@ -37,3 +37,29 @@ void Core::CoreClose()
 {
     _window.close();
 }
+
+void Core::run()
+{
+    Input input = Input();
+
+	input.loadFromFile("../assets/input.ini");
+    start();
+    while (isOpen()) {
+        _time.update();
+        sf::Event event;
+
+        while (CoreEvent(event)) {
+            if (event.type == sf::Event::Closed)
+				CoreClose();
+			if (event.type == sf::Event::KeyPressed)
+				_input.___push_key(event.key.code);
+			if (event.type == sf::Event::KeyReleased)
+				_input.___remove_key(event.key.code);
+        }
+        CoreClear(sf::Color::Black);
+        render();
+        CoreDisplay();
+    }
+    destroy();
+    _window.close();
+}
