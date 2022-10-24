df1c2a58fbf74ca6d2d864729bb5ff7461d29241
NotASurgele <alexis.picard@epitech.eu>
2022-10-24 22:24:40 +0400

    impleted a App class for easier Application creation

--- main.cpp
+++ main.cpp
@@ -8,44 +8,12 @@
 #include "Engine/RenderWindow.hpp"
 #include "Engine/Entity.hpp"
 #include "Engine/Input.hpp"
+#include "Game/App.hpp"
 
 int main(void)
 {
-	Entity e = Entity();
-	Core c("hello world");
-	Input input = Input();
+	App app("hello world");
 
-	input.loadFromFile("../assets/input.ini");
-	R_ADD_RESSOURCE(sf::Texture, "player", "../assets/B_witch_attack.png");
-	e.addComponent<Sprite>()->setTexture(R_GET_RESSOURCE(sf::Texture, "player"));
-	while (c.isOpen()) {
-		sf::Event event;
-		Core::getTime().update();
-
-		//Event call
-		while (c.CoreEvent(event)) {
-			if (event.type == sf::Event::Closed)
-				c.CoreClose();
-			if (event.type == sf::Event::KeyPressed)
-				input.___push_key(event.key.code);
-			if (event.type == sf::Event::KeyReleased)
-				input.___remove_key(event.key.code);
-		}
-		if (Input::isActionPressed("MoveDown"))
-			e.getComponent<Transform2D>()->position.y += 100.f * Core::getDeltaTime();
-		if (Input::isActionPressed("MoveUp"))
-			e.getComponent<Transform2D>()->position.y -= 100.f * Core::getDeltaTime();
-		if (Input::isActionPressed("MoveLeft"))
-			e.getComponent<Transform2D>()->position.x -= 100.f * Core::getDeltaTime();
-		if (Input::isActionPressed("MoveRight"))
-			e.getComponent<Transform2D>()->position.x += 100.f * Core::getDeltaTime();
-		//Update call
-		e.getComponent<Sprite>()->update();
-
-		//Draw call
-		c.CoreClear(sf::Color::Black);
-		c.CoreDraw(e.getComponent<Sprite>());
-		c.CoreDisplay();
-	}
+	app.run();
 	return 0;
 }
