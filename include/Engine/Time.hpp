6b29a793b85ab3772175205937fceacb0fe7b45b
NotaSurgele <alexis.picard@epitech.eu>
2022-10-25 09:36:25 +0400

    change deltaTime access

--- include/Engine/Time.hpp
+++ include/Engine/Time.hpp
@@ -3,13 +3,13 @@
 
 class Time {
     public:
+        static inline float deltaTime = 0.0f;
+
         Time();
         ~Time() = default;
 
         void update();
-        static float getDeltaTime();
 
     private:
         sf::Clock _clock;
-        static inline float deltaTime = 0.0f;
 };
