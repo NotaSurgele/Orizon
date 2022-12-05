7879e6969ca72f42bd0d455764c505703d32f31d
NotASurgele <alexis.picard@epitech.eu>
2022-12-05 20:04:56 +0400

    try to fixed crash in component system

--- src/Engine/System.cpp
+++ src/Engine/System.cpp
@@ -4,30 +4,34 @@
     void System::force_system()
     {
        for (auto &it : _registry) {
-           Entity *e = *it.second.get();
-           auto velocity = e->getComponent<Velocity<float>>();
-           auto transform = e->getComponent<Transform2D>();
+           try {
+                Entity *e = *it.second.get();
+                auto velocity = e->getComponent<Velocity<float>>();
+                auto transform = e->getComponent<Transform2D>();
 
-           if (!velocity || !transform)
-               continue;
-           transform->position.x += velocity->getX() * Time::deltaTime;
-           transform->position.y += velocity->getY() * Time::deltaTime;
+                transform->position.x += velocity->getX() * Time::deltaTime;
+                transform->position.y += velocity->getY() * Time::deltaTime;
+           } catch (...) {
+               std::cout << "Missing force field component" << std::endl;
+           }
        }
     }
 
     void System::draw_system()
     {
         for (auto &it : _registry) {
-            auto transform = (*it.second.get())->getComponent<Transform2D>();
-            auto sprite = (*it.second.get())->getComponent<Sprite>();
+            try {
+                auto transform = (*it.second.get())->getComponent<Transform2D>();
+                auto sprite = (*it.second.get())->getComponent<Sprite>();
 
-            if (!sprite)
-                continue;
-            if (!transform) {
-                sprite->setTransform(Transform2D::zero());
-            } else
-                sprite->setTransform(transform);
-            DRAW(sprite);
+                if (!transform)
+                    sprite->setTransform(Transform2D::zero());
+                else
+                    sprite->setTransform(transform);
+                DRAW(sprite);
+            } catch(...) {
+                std::cout << "RIP !" << std::endl;
+            }
         }
     }
 
--- src/Game/Scenes/GameScene.cpp
+++ src/Game/Scenes/GameScene.cpp
@@ -7,32 +7,31 @@
 void GameScene::create()
 {
     R_ADD_RESSOURCE(sf::Texture, "hobbit", "../assets/Hobbit/pngs/hobbit_idle_spritesheet.png");
-    e.addComponent<Sprite>(&e, R_GET_RESSOURCE(sf::Texture, "hobbit"), 10, 10);
+    //e.addComponent<Sprite>(&e, R_GET_RESSOURCE(sf::Texture, "hobbit"), 10, 10);
     e.addComponent<Animator>(&e)->newAnimation(4, AnimatorRect{0, 0, 64, 64}, .2f, "idle");
     //e.addComponent<Transform2D>();
-    e.addComponent<Velocity<float>>();
     System::addEntity<Entity>(&e);
 }
 
 void GameScene::update()
 {
-    auto transform = e.getComponent<Transform2D>();
-    const float deltaTime = Time::deltaTime;
+   // auto transform = e.getComponent<Transform2D>();
+   // const float deltaTime = Time::deltaTime;
 
-    if (Input::isActionPressed("MoveDown"))
-        transform->position.y += 100.0f * deltaTime;
-    if (Input::isActionPressed("MoveUp"))
-        transform->position.y += -100.0f * deltaTime;
-    if (Input::isActionPressed("MoveLeft"))
-        transform->position.x += -100.0f * deltaTime;
-    if (Input::isActionPressed("MoveRight"))
-        transform->position.x += 100.0f * deltaTime;
+   // if (Input::isActionPressed("MoveDown"))
+   //     transform->position.y += 100.0f * deltaTime;
+   // if (Input::isActionPressed("MoveUp"))
+   //     transform->position.y += -100.0f * deltaTime;
+   // if (Input::isActionPressed("MoveLeft"))
+   //     transform->position.x += -100.0f * deltaTime;
+   // if (Input::isActionPressed("MoveRight"))
+   //     transform->position.x += 100.0f * deltaTime;
     if (Input::isActionPressed("Exit"))
         CLOSE();
     if (Input::isActionPressed("Refresh"))
         CORE->loadInputFromFile(INPUT_FILE);
-    if (Input::isActionPressed("Animate"))
-        e.getComponent<Animator>()->playAnimation("idle", true);
+    //if (Input::isActionPressed("Animate"))
+    //    e.getComponent<Animator>()->playAnimation("idle", true);
 }
 
 void GameScene::destroy()
