c674e375b45e1389f53466ebe0352770051193cb
NotASurgele <alexis.picard@epitech.eu>
2022-12-31 17:19:27 +0400

    remove comment

--- include/Engine/Components/IComponent.hpp
+++ include/Engine/Components/IComponent.hpp
@@ -18,28 +18,28 @@ class IComponent {
     virtual void destroy() = 0;
 };
 
-// template <class... Args>
-// class ComponentFactory {
-//     public:
+template <class... Args>
+class ComponentFactory {
+    public:
 
-//         static std::pair<const char *, IComponent *> create_component(std::string const &name, Entity *e, Args... args)
-//         {
-//             for (auto &it : _construct_map) {
-//                 if (it.first == name)
-//                     return it.second(e, args ...);
-//             }
-//         }
+        static std::pair<const char *, IComponent *> create_component(std::string const &name, Entity *e, Args... args)
+        {
+            for (auto &it : _construct_map) {
+                if (it.first == name)
+                    return it.second(e, args ...);
+            }
+        }
 
-//         static std::pair<const char *, IComponent *> create_transform(Entity *e, Args ... args)
-//         {
-//             Transform2D *transform = new Transform2D(e, args ...);
-//             std::string signature = SIGNATURE(Transform2D);
+        static std::pair<const char *, IComponent *> create_transform(Entity *e, Args ... args)
+        {
+            Transform2D *transform = new Transform2D(e, args ...);
+            std::string signature = SIGNATURE(Transform2D);
 
-//             return std::make_pair<const char *, IComponent *>(signature.c_str(), transform);
-//         }
+            return std::make_pair<const char *, IComponent *>(signature.c_str(), transform);
+        }
 
-//     private:
-//         static inline std::unordered_map<const char *, std::function<void>(Entity *e, Args...)> _construct_map = {
-//             { "Transform2D", &create_transform }
-//         };
-// };
+    private:
+        static inline std::unordered_map<const char *, std::function<void>(Entity *e, Args...)> _construct_map = {
+            { "Transform2D", &create_transform }
+        };
+};
