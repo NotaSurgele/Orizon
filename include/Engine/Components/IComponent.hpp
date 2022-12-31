f197ebc92bd8bda7ec9cb19f174330c83f12ff02
NotASurgele <alexis.picard@epitech.eu>
2022-12-31 19:50:49 +0400

    remove factory removed put the string in brute in the entity class waiting for a better idea

--- include/Engine/Components/IComponent.hpp
+++ include/Engine/Components/IComponent.hpp
@@ -3,12 +3,12 @@
 #include <map>
 #include <functional>
 #include <string>
+#include <iostream>
 
 #define SIGNATURE(type) \
     typeid(type).name()
 
 class Entity;
-class Transform2D;
 
 class IComponent {
   public:
@@ -18,28 +18,12 @@ class IComponent {
     virtual void destroy() = 0;
 };
 
-template <class... Args>
 class ComponentFactory {
     public:
 
-        static std::pair<const char *, IComponent *> create_component(std::string const &name, Entity *e, Args... args)
-        {
-            for (auto &it : _construct_map) {
-                if (it.first == name)
-                    return it.second(e, args ...);
-            }
-        }
+        template <class... Args>
+        static void create_component(std::string const &name, Entity *e, Args... args);
 
-        static std::pair<const char *, IComponent *> create_transform(Entity *e, Args ... args)
-        {
-            Transform2D *transform = new Transform2D(e, args ...);
-            std::string signature = SIGNATURE(Transform2D);
-
-            return std::make_pair<const char *, IComponent *>(signature.c_str(), transform);
-        }
-
-    private:
-        static inline std::unordered_map<const char *, std::function<void>(Entity *e, Args...)> _construct_map = {
-            { "Transform2D", &create_transform }
-        };
+        template <class... Args>
+        static void create_transform(Entity *e, Args... args);
 };
--- src/Engine/Components/Sprite.cpp
+++ src/Engine/Components/Sprite.cpp
@@ -75,3 +75,16 @@ void Sprite::destroy()
     _sprite.~Sprite();
     _texture.~Texture();
 }
+
+template <class... Args>
+void ComponentFactory::create_component(std::string const &name, Entity *e, Args... args)
+{
+    if (name == "Transform2D")
+        return create_transform(e, args ...);
+}
+
+template <class... Args>
+void ComponentFactory::create_transform(Entity *e, Args... args)
+{
+    e->addComponent<Transform2D>(args ...);
+}
