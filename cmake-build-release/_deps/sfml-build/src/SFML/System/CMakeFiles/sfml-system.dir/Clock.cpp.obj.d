2ab66507fe59c0780500828be1fd4e48b34466f2
NotaSurgele <alexis.picard@epitech.eu>
2023-09-25 04:23:29 +0200

    [ADD] can now apply different lighting effect on sprite

---
+++ cmake-build-release/_deps/sfml-build/src/SFML/System/CMakeFiles/sfml-system.dir/Clock.cpp.obj.d
@@ -0,0 +1,7 @@
+_deps/sfml-build/src/SFML/System/CMakeFiles/sfml-system.dir/Clock.cpp.obj: \
+ C:\Users\alexi\OneDrive\Bureau\Dev\Orizon\cmake-build-release\_deps\sfml-src\src\SFML\System\Clock.cpp \
+ C:/Users/alexi/OneDrive/Bureau/Dev/Orizon/cmake-build-release/_deps/sfml-src/include/SFML/System/Clock.hpp \
+ C:/Users/alexi/OneDrive/Bureau/Dev/Orizon/cmake-build-release/_deps/sfml-src/include/SFML/System/Export.hpp \
+ C:/Users/alexi/OneDrive/Bureau/Dev/Orizon/cmake-build-release/_deps/sfml-src/include/SFML/Config.hpp \
+ C:/Users/alexi/OneDrive/Bureau/Dev/Orizon/cmake-build-release/_deps/sfml-src/include/SFML/System/Time.hpp \
+ C:/Users/alexi/OneDrive/Bureau/Dev/Orizon/cmake-build-release/_deps/sfml-src/src/SFML/System/Win32/ClockImpl.hpp
