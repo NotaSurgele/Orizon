fafb802ec8a7946c888f9547b058ff8fad5af4e1
NotaSurgele <alexis.picard@epitech.eu>
2024-01-10 16:17:59 +0900

    [ADD] gravity tree node

--- include/Engine/Components/Gravity.hpp
+++ include/Engine/Components/Gravity.hpp
@@ -6,8 +6,9 @@ class Gravity : public IComponent {
 public:
     Gravity(Entity *e, double const& value=9.81f) : force(value) {}
     ~Gravity() = default;
+
     void destroy() override final {};
 
 public:
-    const float force = 9.81f;
+    float force = 9.81f;
 };
