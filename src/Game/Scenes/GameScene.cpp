be47540aafe2d56b034491df5aff23b65c6249ac
NotaSurgele <alexis.picard@epitech.eu>
2024-01-12 17:37:43 +0900

    [ADD] can now correctly destroy tilemap from script

--- src/Game/Scenes/GameScene.cpp
+++ src/Game/Scenes/GameScene.cpp
@@ -37,12 +37,12 @@ void GameScene::create()
     c.a = 40;
     //player->getComponent<Light>()->setColor(c);
     //player->getComponent<Sprite>()->setShadowIntensity(1);
-    tiledMap = new TiledMap();
+/*    tiledMap = new TiledMap();
     tiledMap->load("../assets/map_test.tmj");
     player->addCustomComponent<CharacterController>(tiledMap);
     player->getComponent<BoxCollider>()->onCollision([&](BoxCollider *other) {
         other->entity()->destroy();
-    });
+    });*/
 }
 
 void GameScene::update()
