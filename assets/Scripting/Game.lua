728ca061fe34aec2e8fab569852221a8ee37dca7
NotaSurgele <alexis.picard@epitech.eu>
2024-04-04 14:57:25 +0900

    [FIX] enemies manager spawing enemy

--- assets/Scripting/Game.lua
+++ assets/Scripting/Game.lua
@@ -1,16 +1,20 @@
 local Player = require 'assets.Scripting.Player'
+local EnemyManager = require 'assets.Scripting.EnemyManager'
 
 local player = Player.new()
-local enemyManager = nil
+local enemyManager = EnemyManager.new()
 
 function Start()
     player:Start()
+    enemyManager:Start()
 end
 
 function Update()
     player:Update()
+    enemyManager:Update()
 end
 
 function Destroy()
-    player:Destroy()
+    --player:Destroy()
+    --enemyManager:Destroy()
 end
